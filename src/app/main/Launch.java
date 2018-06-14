cdbf202da37392ae1e17e9b213973b63815b535d
Pedro Bernardi Alves <pedrobealves@gmail.com>
2018-06-14 16:01:22 -0300

    init commit

---
+++ src/app/main/Launch.java
@@ -0,0 +1,31 @@
+package app.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Launch extends Application {
+
+    public static Stage stage = null;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/app/view/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        this.stage = stage;
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
